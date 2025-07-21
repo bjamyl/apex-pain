c6574c32672fea96f3e1a82ae15bdc5b65d2958e
bjamyl <alhassanjamil0@gmail.com>
2025-07-21 16:15:15 +0000

    fix: fix text

--- components/Services.tsx
+++ components/Services.tsx
@@ -13,7 +13,7 @@ export default function Services() {
               Our Practice Areas and Expertise
             </h3>
             <p className="my-5 md:mb-0 text-slate-500">
-              We provide several intervention pain services in Bangor
+              We provide several <span className="text-bold">interventional</span> pain services in Bangor
             </p>
           </div>
           <Link
